32ad05cba9423c7083f885e4858a62d2811dcbfc
balajiblazer555 <64458006+balajiblazer555@users.noreply.github.com>
2022-10-05 12:29:56 +0530

    Update FibonacciSeries.java

--- day2/FibonacciSeries.java
+++ day2/FibonacciSeries.java
@@ -3,20 +3,18 @@ package week1.day2;
 public class FibonacciSeries {
 
 
-	 
-
 	public static void main(String[] args) {
 
-		int range = 10;
+		int range = 8;
 		int firstNum = 0;
 		int secNum=1;
-		int sum=0;
+		
 		
 		System.out.println("the fibonacci series for the input range: "+range);
 		System.out.print(firstNum+","+secNum+",");
 		
-		for(int i=1; i<=range-2; i++) {
-			sum = firstNum+secNum;
+		for(int i=1; i<range-1; i++) {
+			int sum = firstNum+secNum;
 			firstNum=secNum;
 			secNum= sum;
 			System.out.print(sum+",");	
