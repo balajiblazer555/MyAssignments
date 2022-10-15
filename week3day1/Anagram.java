5bf51c0dc44967e7e5f897f9e8834deef5fb8a0c
balajiblazer555 <64458006+balajiblazer555@users.noreply.github.com>
2022-10-15 19:11:19 +0530

    Update Anagram.java

--- week3day1/Anagram.java
+++ week3day1/Anagram.java
@@ -5,45 +5,28 @@ import java.util.Arrays;
 public class Anagram {
 
 	public static void main(String[] args) {
-		// Declare a String text1 = "stops";
 		String text1 = "stops";
-
-		// Declare another String text2 ="potss";
 		String text2 = "potss";
-
-		// a) Check length of the strings are same then (Use A Condition)
-		int a = text1.length();
-		int b = text2.length();
-		if (a == b) {
-			System.out.println("Length of both strings are same");
-		} else {
-			System.out.println("Length of both strings are not same");
-		}
-
-		// b) Convert both Strings in to characters
-		char[] char1 = text1.toCharArray();
-		char[] char2 = text2.toCharArray();
-
-		// c) Sort Both the arrays
-		Arrays.sort(char1);// [s,t,o,p,s]
-		Arrays.sort(char2);// [s,t,o,p,s]
 		int count = 0;
-
-		// d) Check both the arrays has same value
-
-		for (int i = 0; i < char1.length; i++) {
-
-			if (char1[i] != char2[i]) {
-				count++;
-				break;
-			}
-
+		if (text1.length() == text2.length()) {
+			System.out.println("Length of the strings are same");
 		}
-		if (count > 0) {
-			System.out.println("both are not same");
+		char[] a1 = text1.toCharArray();
+		char[] a2 = text2.toCharArray();
+
+		Arrays.sort(a1);
+		Arrays.sort(a2);
+
+		for (int i = 0; i < a1.length; i++) {
+			if (a1[i] == a2[i]) {
+				count++;
+			}
+		}
+		if (count == a2.length) {
+			System.out.println("Both the arrays has same value");
 
 		} else {
-			System.out.println("both arrays are same");
+			System.out.println("Both the arrays has not same value");
 		}
 	}
 }
