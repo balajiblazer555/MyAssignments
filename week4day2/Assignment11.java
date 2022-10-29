b5c3ff59f833390d0030134577a2f27ae742c26a
balajiblazer555 <64458006+balajiblazer555@users.noreply.github.com>
2022-10-29 08:34:20 +0530

    Update Assignment11.java

--- week4day2/Assignment11.java
+++ week4day2/Assignment11.java
@@ -3,7 +3,6 @@ package week4.day2;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -18,17 +17,16 @@ public class Assignment11 {
 		driver.get("https://jqueryui.com/droppable");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().window().maximize();
-		
+
+		Actions act = new Actions(driver);
+		WebElement scroll = driver.findElement(By.xpath("//li/a[text()='Checkboxradio']"));
+		act.scrollToElement(scroll).perform();
+
 		WebElement frame = driver.findElement(By.xpath("//iframe[@class='demo-frame']"));
 		driver.switchTo().frame(frame);
-		
+
 		WebElement source = driver.findElement(By.xpath("//div[@id='draggable']/p"));
 		WebElement target = driver.findElement(By.xpath("//div[@id='droppable']/p"));
-		Actions act=new Actions(driver);
 		act.dragAndDrop(source, target).perform();
-		act.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).perform();
-		act.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).perform();
-
 	}
-
 }
